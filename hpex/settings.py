a1902b26103ae7f2941d4f4532dd5436d5013f6b
Aiomedin Chorobaev <eliothab58@gmail.com>
2024-04-30 20:38:40 +0600

    change static and media path

--- hpex/settings.py
+++ hpex/settings.py
@@ -129,9 +129,9 @@ if DEBUG:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 else:
-    MEDIA_ROOT = '/var/www/tododo/media'
+    MEDIA_ROOT = '/var/www/hpex/media'
     MEDIA_URL = '/media/'
-    STATIC_ROOT = '/var/www/tododo/static'
+    STATIC_ROOT = '/var/www/hpex/static'
     STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
